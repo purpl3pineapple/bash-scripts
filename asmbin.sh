4f949cc3469b5b1cbc49b7525fad3bd8c68c7eae
Payne Warren <88871686+purpl3pineapple@users.noreply.github.com>
2025-02-11 14:23:40 -0500

    Update asmbin.sh

--- asmbin.sh
+++ asmbin.sh
@@ -3,12 +3,12 @@
 # Extract binary code from a given assembly file
 
 if [ -z "$1" ]; then
-    echo "Usage: asmbin [NAME_OF_ASMFILE]"
+    echo "Usage: asmbin [ASMFILE_NAME]"
     echo "NOTE: Do not include the file's extension in the name"
     exit 1
 fi
 
-as -o "${1}.o" "${1}.s" && objcopy --dump-section .text=$1 "${1}.o"
+as -o "$1.o" "$1.s" && objcopy --dump-section .text=$1 "$1.o"
 
 if [ $? -eq 0 ]; then
     echo "Successfully created binary $1 in $(pwd)"
