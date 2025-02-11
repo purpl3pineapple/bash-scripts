ebf87ecb5de1a175814199f9154c6859dce4c6ef
Payne Warren <88871686+purpl3pineapple@users.noreply.github.com>
2025-02-11 13:43:04 -0500

    Create mkasm.sh

---
+++ mkasm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple script to create a new assembly file
+# Includes Intel syntax header
+
+if [ -z "$1" ]
+        echo "Usage: mkasm [FILENAME]"
+        exit 1
+fi
+
+echo "\t.intel_syntax\tnoprefix\n\t.global\t_start\n\n_start:\n" >> "$1.s"
+
+exit 0
