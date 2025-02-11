ef82d20fdb3db6135e2222d93b9166d8e8184954
Payne Warren <88871686+purpl3pineapple@users.noreply.github.com>
2025-02-11 14:18:09 -0500

    Update mkasm.sh

--- mkasm.sh
+++ mkasm.sh
@@ -4,10 +4,10 @@
 # Includes Intel syntax header
 
 if [ -z "$1" ]; then
-        echo "Usage: mkasm [FILENAME]"
+        echo "Usage: mkasm [NAME]"
         exit 1
 fi
 
-echo -e "\t.intel_syntax\tnoprefix\n\t.global\t_start\n\n_start:\n" >> "$1.s"
+echo -e "\t.intel_syntax\tnoprefix\n\t.global\t_start\n\n_start:\n" >> "$1.asm"
 
 exit 0
